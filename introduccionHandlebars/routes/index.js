88279a735c1c6d362e960ab2be51193eacb89e6e
Liliana Herrera <122304941+llherrerap@users.noreply.github.com>
2023-11-08 21:26:52 -0500

    Introducción Handlebars

---
+++ introduccionHandlebars/routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //El metodo render renderiza la plantilla
+  //En este caso la plantilla es index.hbs
+  //A la plantilla index se le manda una información que este caso es curso
+  // res.render('index', { curso: 'Curso Backend' });
+
+  const curso = {
+    nombre: 'Desarrollo Web Backend Básico',
+    profesor: 'Liliana Herrera',
+    correo: 'lherrera@udistrital.edu.co',
+    horario: 'L-V 7:00PM a 9:30PM'
+  }
+  res.render('index', curso)
+});
+
+module.exports = router;
