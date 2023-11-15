4d8f1ee75488a1007883248e48d023df9a09628c
Liliana Herrera <122304941+llherrerap@users.noreply.github.com>
2023-11-14 21:13:57 -0500

    Enrutamiento docentes y estudiantes

--- API-REST/routes/index.js
+++ API-REST/routes/index.js
@@ -3,12 +3,22 @@ var router = express.Router();
 const { conexion } = require('../database/conexion')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/docentes', function(req, res, next) {
   conexion.query('SELECT * FROM docentes;', (error, resultado) => {
     if (error) {
       res.status(500).send('Ocurrio un error en la consulta')
     } else {
       res.status(200).render('index', { resultado, title: 'Docentes' })
+    }
+  })
+});
+
+router.get('/estudiantes', function(req, res, next) {
+  conexion.query('SELECT * FROM estudiantes;', (error, resultado) => {
+    if (error) {
+      res.status(500).send('Ocurrio un error en la consulta')
+    } else {
+      res.status(200).render('index', { resultado, title: 'Estudiantes' })
     }
   })
 });
