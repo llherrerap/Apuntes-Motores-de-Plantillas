f7d5353e99cce665295712bbc6d355a4757e43a0
Liliana Herrera <122304941+llherrerap@users.noreply.github.com>
2023-11-15 19:35:37 -0500

    Enrutamiento y formulario para agregar docentes

--- API-REST/routes/index.js
+++ API-REST/routes/index.js
@@ -4,6 +4,7 @@ const { conexion } = require('../database/conexion')
 
 /* GET home page. */
 router.get('/docentes', function(req, res, next) {
+  // Consulta de docentes en la base de datos por medio del comando SELECT
   conexion.query('SELECT * FROM docentes;', (error, resultado) => {
     if (error) {
       res.status(500).send('Ocurrio un error en la consulta')
@@ -12,6 +13,10 @@ router.get('/docentes', function(req, res, next) {
     }
   })
 });
+
+router.get('/registro-docente', (req, res) => {
+  res.sendFile('registro-docentes.html', {root: 'public'})
+})
 
 router.get('/estudiantes', function(req, res, next) {
   conexion.query('SELECT * FROM estudiantes;', (error, resultado) => {
