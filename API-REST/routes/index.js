321db587b786dbd7dc6a0f750d58ca3868cbbce1
Liliana Herrera <122304941+llherrerap@users.noreply.github.com>
2023-11-15 19:52:58 -0500

    Registro de docentes en la base de datos

--- API-REST/routes/index.js
+++ API-REST/routes/index.js
@@ -18,6 +18,22 @@ router.get('/registro-docente', (req, res) => {
   res.sendFile('registro-docentes.html', {root: 'public'})
 })
 
+router.post('/agregar-docente', (req, res) => {
+  const nombre = req.body.name
+  const apellido = req.body.lastname
+  const fecha = req.body.date
+  const genero = req.body.genero
+  const direccion = req.body.direccion
+  conexion.query(`INSERT INTO docentes (nombre, apellido, fecha_nacimiento, genero, direccion) VALUES ('${nombre}', '${apellido}', '${fecha}', '${genero}', '${direccion}')`, (error, resultado) => {
+    if(error){
+      console.log(`Ocurrio un error en la ejecución ${error}`)
+      res.status(500).send('Error en la consulta')
+    } else {
+      res.status(200).redirect('/docentes')
+    }
+  })
+})
+
 router.get('/estudiantes', function(req, res, next) {
   conexion.query('SELECT * FROM estudiantes;', (error, resultado) => {
     if (error) {
